7041ab1496e9b50bb149f54e11522efd0ed3ec0d
Aarin Bailey <aarin.m.bailey@gmail.com>
2025-03-08 17:20:57 -0600

    Update

--- api/user/user_router.py
+++ api/user/user_router.py
@@ -3,6 +3,9 @@ from typing import List
 from sqlalchemy.orm import Session
 from db_config.db_connection import SessionLocal
 
+
+router = APIRouter(prefix="/api")
+
 # Utility function for getting the DB session
 def get_db():
     db = SessionLocal()
