868d72c263ba0c891632fc565c377bc23e44b32b
Aarin Bailey <aarin.m.bailey@gmail.com>
2025-03-08 17:19:00 -0600

    Update

--- api/client/client_get.py
+++ api/client/client_get.py
@@ -4,8 +4,9 @@ from datetime import datetime, timedelta
 from fastapi import APIRouter, Depends, HTTPException
 from typing import List
 from sqlalchemy.orm import Session
-from api.user.user_router import get_db, router
+from api.user.user_router import get_db
 from models.models import Client, Organization
+from root.root_elements import router
 from schemas.schemas import ClientSchema
 
 
