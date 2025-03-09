15d1fc594a418ba77ca4a505fc3fee700a326191
Aarin Bailey <aarin.m.bailey@gmail.com>
2025-03-08 23:45:53 -0600

    Update

--- api/templates/template_router.py
+++ api/templates/template_router.py
@@ -15,9 +15,10 @@ logger = logging.getLogger(__name__)
 
 
 @router.get("/templates", response_model=List[schemas.Template])
-def read_templates(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    templates = db.query(Template).order_by(Template.id.asc()).offset(skip).limit(limit).all()
+def read_templates(db: Session = Depends(get_db)):
+    templates = db.query(Template).all()  # Fetch all templates without pagination
     return templates
+
 
 
 
