14a93a7984901e1e6ce3872b183b1c922a793067
Aarin Bailey <aarin.m.bailey@gmail.com>
2025-03-10 19:29:46 -0500

    Update

--- models/__init__.py
+++ models/__init__.py
@@ -49,6 +49,7 @@ BrandingSettings.client = relationship("Client", back_populates="branding_settin
 
 from models.clientModel.sla_model import SLA, SLACondition, Priority, Impact
 from models.clientModel.billingagreement_model import BillingAgreement, BillingAgreementItem
+from models.clientModel.csat_model import Survey
 from models.models import Client
 from models.models import Organization
 from models.clientModel.ticket_model import Ticket
