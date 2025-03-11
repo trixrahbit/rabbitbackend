842c05509c450671e19736a26853de8668bc8c10
Aarin Bailey <aarin.m.bailey@gmail.com>
2025-03-10 19:51:48 -0500

    Update

--- api/subscription/subscription_router.py
+++ api/subscription/subscription_router.py
@@ -9,23 +9,26 @@ from root.root_elements import router
 from schemas.subscription.subscription_schema import SubscriptionPlanCreate, SubscriptionCreate, SubscriptionPlan, Subscription
 
 
-@router.get("/organizations/{client_id}/subscriptions", response_model=List[Subscription])
-async def read_subscriptions(client_id: int, db: Session = Depends(get_db)):
-    subscriptions = db.query(Organization).filter(Organization.clients_id == client_id).first().subscriptions
+@router.get("/organizations/{org_id}/subscriptions", response_model=List[Subscription])
+async def read_subscriptions(org_id: int, db: Session = Depends(get_db)):
+    subscriptions = db.query(Organization).filter(Organization.id == org_id).first().subscriptions
     return subscriptions
 
-
-@router.post("/organizations/{client_id}/subscriptions", response_model=Subscription)
-async def create_subscription(client_id: int, subscription: SubscriptionCreate, db: Session = Depends(get_db)):
+@router.post("/organizations/{org_id}/subscriptions", response_model=Subscription)
+async def create_subscription(org_id: int, subscription: SubscriptionCreate, db: Session = Depends(get_db)):
     subscription = SQLASubscription(**subscription.dict())
     db.add(subscription)
     db.commit()
     db.refresh(subscription)
     return subscription
 
-@router.delete("/organizations/{client_id}/subscriptions/{subscription_id}", response_model=Subscription)
-async def delete_subscription(client_id: int, subscription_id: int, db: Session = Depends(get_db)):
-    subscription = db.query(Organization).filter(Organization.clients_id == client_id).first().subscriptions.filter(SQLASubscription.id == subscription_id).first()
-    db.delete(subscription)
+@router.patch("/organizations/{org_id}/subscriptions/{subscription_id}", response_model=Subscription)
+async def update_subscription(org_id: int, subscription_id: int, subscription: SubscriptionCreate, db: Session = Depends(get_db)):
+    db_subscription = db.query(Organization).filter(Organization.id == org_id).first().subscriptions.filter(SQLASubscription.id == subscription_id).first()
+    update_data = subscription.dict(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(db_subscription, key, value)
     db.commit()
-    return subscription
+    db.refresh(db_subscription)
+    return db_subscription
+
