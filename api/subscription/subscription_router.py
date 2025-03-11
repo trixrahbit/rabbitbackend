3af7682f0674a9460f5cbe2171f6324c7662e28d
Aarin Bailey <aarin.m.bailey@gmail.com>
2025-03-10 19:46:00 -0500

    Update

--- api/subscription/subscription_router.py
+++ api/subscription/subscription_router.py
@@ -9,28 +9,23 @@ from root.root_elements import router
 from schemas.subscription.subscription_schema import SubscriptionPlanCreate, SubscriptionCreate, SubscriptionPlan, Subscription
 
 
-@router.post("/subscription-plans/", response_model=SubscriptionPlan)
-def create_subscription_plan(plan: SubscriptionPlanCreate, db: Session = Depends(get_db)):
-    db_plan = SQLASubscriptionPlan(**plan.dict())
-    db.add(db_plan)
-    db.commit()
-    db.refresh(db_plan)
-    return db_plan  # FastAPI will convert this SQLAlchemy model to a Pydantic model
+@router.get("/organizations/{client_id}/subscriptions", response_model=List[Subscription])
+async def read_subscriptions(client_id: int, db: Session = Depends(get_db)):
+    subscriptions = db.query(Organization).filter(Organization.client_id == client_id).first().subscriptions
+    return subscriptions
 
-@router.post("/subscriptions/", response_model=Subscription)
-def create_subscription(subscription: SubscriptionCreate, db: Session = Depends(get_db)):
-    db_subscription = SQLASubscription(**subscription.dict())
-    db.add(db_subscription)
-    db.commit()
-    db.refresh(db_subscription)
-    return db_subscription  # FastAPI will convert this SQLAlchemy model to a Pydantic model
 
-@router.get("/clients/{client_id}/subscriptions", response_model=List[Subscription])
-def get_client_subscriptions(client_id: int, db: Session = Depends(get_db)):
-    subscriptions = (
-        db.query(SQLASubscription)
-        .join(Organization)
-        .filter(Organization.client_id == client_id)
-        .all()
-    )
-    return subscriptions  # FastAPI will convert this list of SQLAlchemy models to a list of Pydantic models
+@router.post("/organizations/{client_id}/subscriptions", response_model=Subscription)
+async def create_subscription(client_id: int, subscription: SubscriptionCreate, db: Session = Depends(get_db)):
+    subscription = SQLASubscription(**subscription.dict())
+    db.add(subscription)
+    db.commit()
+    db.refresh(subscription)
+    return subscription
+
+@router.delete("/organizations/{client_id}/subscriptions/{subscription_id}", response_model=Subscription)
+async def delete_subscription(client_id: int, subscription_id: int, db: Session = Depends(get_db)):
+    subscription = db.query(Organization).filter(Organization.client_id == client_id).first().subscriptions.filter(SQLASubscription.id == subscription_id).first()
+    db.delete(subscription)
+    db.commit()
+    return subscription
